fefca637d3247fd285701e72475e8f48417526c0
Sambor Witek <63224@ue.poznan.pl>
2023-12-12 05:58:41 +0100

    lol

---
+++ moduł1.py
@@ -0,0 +1,19 @@
+import requests
+
+
+api_key = RGAPI-fa205826-b426-417b-9cb6-2254f44355b5
+api_url = 'https://api.example.com/data'
+
+headers = {
+    'Authorization': f'Bearer {api_key}',
+    'Content-Type': 'application/json',
+}
+
+# Make a GET request to the API
+response = requests.get(api_url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code} - {response.text}")
